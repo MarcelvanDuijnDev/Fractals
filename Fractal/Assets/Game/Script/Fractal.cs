b52ea58bbbf033474c37aaed9cd9fd86fc7507a6
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2019-02-18 00:41:57 +0100

    Base Project

---
+++ Fractal/Assets/Game/Script/Fractal.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _IncreaseText;
+    [SerializeField] private GameObject _ParentObject;
+    [SerializeField] private GameObject _LRPrefab;
+    [SerializeField] private float _CircleSize;
+    [SerializeField] private int _Iterations;
+    [Header("Settings")]
+    [SerializeField] private float _Increase;
+    [SerializeField] private float _UpdateSpeed;
+    [SerializeField] private bool _AutoIncrease;
+    [SerializeField] private float _XIncrease;
+    [SerializeField] private float _YIncrease;
+
+
+    private Vector3[] _CirclePoints;
+    private float _CheckIncreaseAmount = -1;
+    private LineRenderer[] _LineR;
+
+    void Start()
+    {
+        _LineR = new LineRenderer[_Iterations];
+        for (int i = 0; i < _Iterations; i++)
+        {
+            GameObject obj = Instantiate(_LRPrefab);
+            obj.transform.parent = _ParentObject.transform;
+            _LineR[i] = obj.GetComponent<LineRenderer>();
+        }
+        _CirclePoints = GetPoints();
+    }
+
+    void Update()
+    {
+        if(_CheckIncreaseAmount != _Increase)
+        {
+            _IncreaseText.text = _Increase.ToString();
+            StartCoroutine(UpdateNewPoints());
+            _CheckIncreaseAmount = _Increase;
+            _CirclePoints = GetPoints();
+        }
+    }
+
+    private IEnumerator UpdateNewPoints()
+    {
+        for (int i = 0; i < _Iterations; i++)
+        {
+            _LineR[i].SetPositions(GetPointsUsing(i));
+           yield return new WaitForSeconds(_UpdateSpeed);
+        }
+        //yield return new WaitForSeconds(_UpdateSpeed);
+        if (_AutoIncrease)
+        _Increase += 1;
+    }
+
+    private Vector3[] GetPoints()
+    {
+        _CirclePoints = new Vector3[Mathf.RoundToInt(_Iterations)];
+        for (int i = 0; i < _Iterations; i++)
+        {
+            float angle = i * Mathf.PI * 2f / _Iterations;
+            _CirclePoints[i] = new Vector3(Mathf.Cos(angle * _XIncrease), 0, Mathf.Sin(angle * _YIncrease)) * _CircleSize;
+        }
+        return _CirclePoints;
+    }
+
+    private Vector3[] GetPointsUsing(float current)
+    {
+        Vector3[] points = new Vector3[2];
+        points[0] = _CirclePoints[Mathf.RoundToInt(current)];
+
+        if (current * _Increase >= _Iterations - 1)
+        {
+            points[1] = _CirclePoints[Mathf.RoundToInt((current * _Increase) - ((Mathf.FloorToInt(current * _Increase / _Iterations)) * _Iterations))];
+        }
+        else
+        {
+            points[1] = _CirclePoints[Mathf.RoundToInt(current * _Increase)];
+        }
+
+        return points;
+    }
+}
---
+++ Fractal/Assets/Game/Script/Fractal_Madelbrot.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Fractal_Madelbrot : MonoBehaviour
+{
+    [SerializeField] private float _UpdateSpeed;
+
+    [SerializeField] private Mandelbrot[] _Mandelbrot;
+
+    [SerializeField] private GameObject _PrefabObject;
+    [SerializeField] private GameObject _ParentObject;
+
+    private float _CheckIncrease;
+
+    void Start()
+    {
+        for (int i = 0; i < _Mandelbrot.Length; i++)
+        {
+            CreateNewIteration(i);
+        }
+    }
+
+    void Update()
+    {
+        if(_CheckIncrease != _Mandelbrot[0]._P_Increase)
+        {
+            for (int i = 0; i < _Mandelbrot.Length; i++)
+            {
+                StartCoroutine(SetPoints(i));
+            }
+            _CheckIncrease = _Mandelbrot[0]._P_Increase;
+        }
+    }
+
+    private void CreateNewIteration(int iterationID)
+    {
+        _Mandelbrot[iterationID]._P_CirclePoints = GetPoints(iterationID);
+        _Mandelbrot[iterationID]._P_LineRenderer = new LineRenderer[_Mandelbrot[iterationID]._P_IterationSize];
+
+        GameObject parentObj = Instantiate(_ParentObject);
+        for (int i = 0; i < _Mandelbrot[iterationID]._P_IterationSize; i++)
+        {
+            GameObject obj = Instantiate(_PrefabObject);
+            obj.transform.parent = parentObj.transform;
+            _Mandelbrot[iterationID]._P_LineRenderer[i] = obj.GetComponent<LineRenderer>();
+        }
+        GetPoints(iterationID);
+    }
+
+
+    //Circle Points
+    private Vector3[] GetPoints(int iterationID)
+    {
+        _Mandelbrot[iterationID]._P_CirclePoints = new Vector3[Mathf.RoundToInt(_Mandelbrot[iterationID]._P_IterationSize)];
+        for (int i = 0; i < _Mandelbrot[iterationID]._P_IterationSize; i++)
+        {
+            float angle = i * Mathf.PI * 2 / _Mandelbrot[iterationID]._P_IterationSize;
+            _Mandelbrot[iterationID]._P_CirclePoints[i] = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * _Mandelbrot[iterationID]._P_CircleSize;
+        }
+        return _Mandelbrot[iterationID]._P_CirclePoints;
+    }
+
+    private IEnumerator SetPoints(int iterationID)
+    {
+        for (int i = 0; i < _Mandelbrot[iterationID]._P_LineRenderer.Length; i++)
+        {
+            Vector3[] points = new Vector3[2];
+            points[0] = _Mandelbrot[iterationID]._P_CirclePoints[Mathf.RoundToInt(i)];
+
+            if (i * _Mandelbrot[iterationID]._P_Increase >= _Mandelbrot[iterationID]._P_IterationSize - 1)
+            {
+                points[1] = _Mandelbrot[iterationID]._P_CirclePoints[Mathf.RoundToInt((i * _Mandelbrot[iterationID]._P_Increase) - ((Mathf.FloorToInt(i * _Mandelbrot[iterationID]._P_Increase / _Mandelbrot[iterationID]._P_IterationSize)) * _Mandelbrot[iterationID]._P_IterationSize))];
+            }
+            else
+            {
+                points[1] = _Mandelbrot[iterationID]._P_CirclePoints[Mathf.RoundToInt(i * _Mandelbrot[iterationID]._P_Increase)];
+            }
+            _Mandelbrot[iterationID]._P_LineRenderer[i].SetPositions(points);
+            yield return new WaitForSeconds(_UpdateSpeed);
+        }
+    }
+
+    private Vector3[] GetPointsUsing(int iterationID, int lineRendereID)
+    {
+        Vector3[] points = new Vector3[2];
+        points[0] = _Mandelbrot[iterationID]._P_CirclePoints[Mathf.RoundToInt(lineRendereID)];
+
+        if (lineRendereID * _Mandelbrot[iterationID]._P_Increase >= _Mandelbrot[iterationID]._P_IterationSize - 1)
+        {
+            points[1] = _Mandelbrot[iterationID]._P_CirclePoints[Mathf.RoundToInt((lineRendereID * _Mandelbrot[iterationID]._P_Increase) - ((Mathf.FloorToInt(lineRendereID * _Mandelbrot[iterationID]._P_Increase / _Mandelbrot[iterationID]._P_IterationSize)) * _Mandelbrot[iterationID]._P_IterationSize))];
+        }
+        else
+        {
+            points[1] = _Mandelbrot[iterationID]._P_CirclePoints[Mathf.RoundToInt(lineRendereID * _Mandelbrot[iterationID]._P_Increase)];
+        }
+
+        return points;
+    }
+}
+
+
+[System.Serializable]
+public class Mandelbrot
+{
+    public float _P_CircleSize;
+    public int _P_IterationSize;
+    public int _P_Increase;
+    public Vector3[] _P_CirclePoints;
+    public LineRenderer[] _P_LineRenderer;
+}
