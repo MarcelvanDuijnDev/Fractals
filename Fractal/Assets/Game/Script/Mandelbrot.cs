910c525f4819804243b889e5ffe43df5d6313591
Marcel van Duijn <33446846+MarcelvanDuijnDev@users.noreply.github.com>
2019-02-18 23:00:10 +0100

    Update Mandelbrot

--- Fractal/Assets/Game/Script/Mandelbrot.cs
+++ Fractal/Assets/Game/Script/Mandelbrot.cs
@@ -11,6 +11,8 @@ public class Mandelbrot : MonoBehaviour {
     [SerializeField] private float[] _Values;
 
     private Vector2 _GridPositions;
+    private float _HighestValue;
+    private float _LowestValue;
 
     void Start() {
         _Values = new float[Mathf.RoundToInt(_Grid.x * _Grid.y)];
@@ -28,10 +30,11 @@ public class Mandelbrot : MonoBehaviour {
         for (int i = 0; i < _Grid.y; i++) {
             for (int o = 0; o < _Grid.x; o++) {
                 GameObject obj = Instantiate(_PrefabObject);
-                obj.transform.position = new Vector3(_GridPositions.x + o, _GridPositions.y + i, 0);
+                obj.transform.position = new Vector3(_GridPositions.x + o, 0, _GridPositions.y + i);
                 obj.transform.parent = _Parentobject.transform;
                 _GridObjects[Mathf.RoundToInt((i * _Grid.x) + o)] = obj;
-                float value1 = Mathf.Pow((_GridPositions.x + o), 2) * 0.01f;
+                //float value1 = Mathf.Pow((_GridPositions.x + o), 2) * 0.01f;
+                float value1 = check * 0.01f;
                 float value2 = Mathf.Pow((_GridPositions.y + i), 2) * 0.01f;
                 float value3 = 0;
                 if(value1 < value2) {
@@ -40,6 +43,7 @@ public class Mandelbrot : MonoBehaviour {
                 else {
                     value3 = value1;
                 }
+                check++;
                 _Values[Mathf.RoundToInt((i * _Grid.x) + o)] = value3;
             }
         }
@@ -51,18 +55,25 @@ public class Mandelbrot : MonoBehaviour {
             for (int o = 0; o < _Grid.x; o++) {
 
                 float calcSize = _GridPositions.x + o;
-                for (int p = 0; p < 5; p++) 
+                for (int p = 0; p < 2; p++) 
                 {
-                    _Values[Mathf.RoundToInt((i * _Grid.x) + o)] = Mathf.Pow(_Values[Mathf.RoundToInt((i * _Grid.x) + o)], 2);
+                    _Values[Mathf.RoundToInt((i * _Grid.x) + o)] = Mathf.Pow(_Values[Mathf.RoundToInt((i * _Grid.x) + o)], 2) + -1;
                 }
             }
         }
 
-        for (int i = 0; i < _Values.Length; i++) {
-            if(_Values[i] > 50 || _Values[i] < -50) 
+        for (int i = 0; i < _Values.Length; i++)
+        {
+            if (_Values[i] > _HighestValue)
             {
-                _GridObjects[i].SetActive(false);
+                _HighestValue = _Values[i];
             }
+        }
+
+        float calc = 255 / _HighestValue;
+
+        for (int i = 0; i < _Values.Length; i++) {
+            _GridObjects[i].GetComponent<Renderer>().material.color = new Vector4(0, 0, calc * _Values[i], 1);
         }
     }
 
